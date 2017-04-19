346a41e9f976b32ac909d3ebd29c4717d7ceb5d3
Simon Brungs <00pflaume@gmail.com>
2017-04-19 19:53:33 +0200

    First Upload

---
+++ src/main/java/de/simonsator/partyandfriends/spigot/api/party/PartyManager.java
@@ -0,0 +1,45 @@
+package de.simonsator.partyandfriends.spigot.api.party;
+
+import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayer;
+import de.simonsator.partyandfriends.utilities.disable.Deactivated;
+import org.bukkit.plugin.java.JavaPlugin;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Manages the parties. From here you can get a PlayerParty.
+ * @author Simonsator
+ * @version 1.0.0 19.04.17
+ */
+public class PartyManager extends JavaPlugin implements Deactivated {
+	private static PartyManager instance;
+
+	public PartyManager() {
+		instance = this;
+	}
+
+	@Override
+	public void onEnable() {
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+
+	public static PartyManager getInstance() {
+		return instance;
+	}
+
+	Jedis getConnection() {
+		Jedis jedis = new Jedis(getConfig().getString("Redis.Host"), getConfig().getInt("Redis.Port"));
+		if (!getConfig().getString("Redis.Password").equals(""))
+			jedis.auth(getConfig().getString("Redis.Password"));
+		return jedis;
+	}
+
+	public PlayerParty getParty(PAFPlayer pPlayer) {
+		try (Jedis jedis = getConnection()) {
+			String id = jedis.get("paf:parties:players:" + pPlayer.getUniqueId() + ":id");
+			if (id == null)
+				return null;
+			return new PlayerParty(new Integer(id));
+		}
+	}
+}
---
+++ src/main/java/de/simonsator/partyandfriends/spigot/api/party/PlayerParty.java
@@ -0,0 +1,93 @@
+package de.simonsator.partyandfriends.spigot.api.party;
+
+import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayer;
+import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayerManager;
+import redis.clients.jedis.Jedis;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Represents a party with players
+ * @author Simonsator
+ * @version 1.0.0 19.04.17
+ */
+public class PlayerParty {
+	private final int ID;
+
+	public PlayerParty(int pID) {
+		ID = pID;
+	}
+
+	public boolean isAMember(PAFPlayer pPlayer) {
+		try (Jedis jedis = PartyManager.getInstance().getConnection()) {
+			return jedis.lrange("paf:parties:parties:" + ID + ":players", 0, 1000).contains(pPlayer.getUniqueId().toString());
+		}
+	}
+
+	protected List<UUID> getInvited() {
+		Jedis jedis = PartyManager.getInstance().getConnection();
+		List<String> list = jedis.lrange("paf:parties:parties:" + ID + ":invited", 0, 1000);
+		jedis.close();
+		List<UUID> uuids = new ArrayList<>();
+		for (String uuid : list)
+			uuids.add(UUID.fromString(uuid));
+		return uuids;
+	}
+
+
+	public PAFPlayer getLeader() {
+		Jedis jedis = PartyManager.getInstance().getConnection();
+		String uuid = jedis.get("paf:parties:parties:" + ID + ":leader");
+		jedis.close();
+		if (uuid == null)
+			return null;
+		return PAFPlayerManager.getInstance().getPlayer(UUID.fromString(uuid));
+	}
+
+	public List<PAFPlayer> getPlayers() {
+		List<PAFPlayer> players = new ArrayList<>();
+		Jedis jedis = PartyManager.getInstance().getConnection();
+		for (String uuid : jedis.lrange("paf:parties:parties:" + ID + ":players", 0, 1000)) {
+			players.add(PAFPlayerManager.getInstance().getPlayer(UUID.fromString(uuid)));
+		}
+		jedis.close();
+		return players;
+	}
+
+	public boolean isNobodyInvited() {
+		return getInvited().isEmpty();
+	}
+
+	/**
+	 * @return Returns all players in this party (inclusive the leader).
+	 */
+	public List<PAFPlayer> getAllPlayers() {
+		List<PAFPlayer> allPlayers = getPlayers();
+		PAFPlayer leader = getLeader();
+		if (leader != null)
+			allPlayers.add(leader);
+		return allPlayers;
+	}
+
+	/**
+	 * Returns true if the player is in the party. Returns false if the player
+	 * is not in the party.
+	 *
+	 * @param pPlayer The player
+	 * @return Returns true if the player is in the party. Returns false if the
+	 * player is not in the party.
+	 */
+	public boolean isInParty(PAFPlayer pPlayer) {
+		return isAMember(pPlayer) || pPlayer.getUniqueId().equals(getLeader().getUniqueId());
+	}
+
+	public boolean isLeader(PAFPlayer player) {
+		return getLeader() != null && player != null && this.getLeader().getUniqueId().equals(player.getUniqueId());
+	}
+
+	public boolean isInvited(PAFPlayer pPlayer) {
+		return getInvited().contains(pPlayer.getUniqueId());
+	}
+}
