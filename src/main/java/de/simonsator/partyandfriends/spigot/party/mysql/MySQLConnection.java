4cc4cf1b6c8b9c70fd3ececcc0b23805edf74bda
Simonsator <00pflaume@gmail.com>
2022-08-27 18:31:28 +0200

    Fixed tableExists

--- src/main/java/de/simonsator/partyandfriends/spigot/party/mysql/MySQLConnection.java
+++ src/main/java/de/simonsator/partyandfriends/spigot/party/mysql/MySQLConnection.java
@@ -6,7 +6,10 @@ import de.simonsator.partyandfriends.spigot.communication.sql.SQLCommunication;
 import de.simonsator.partyandfriends.spigot.error.ErrorReporter;
 import de.simonsator.partyandfriends.spigot.pafplayers.mysql.PAFPlayerMySQL;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,17 +20,23 @@ public class MySQLConnection extends SQLCommunication {
 		super(pMySQLData.DATABASE, "jdbc:mysql://" + pMySQLData.HOST + ":" + pMySQLData.PORT, pMySQLData.USERNAME, pMySQLData.PASSWORD, pMySQLData.USE_SSL);
 		this.TABLE_PREFIX = pMySQLData.TABLE_PREFIX;
 		try {
-			if (!tableExists(getConnection(), TABLE_PREFIX + "party"))
-				new ErrorReporter("§cYou did not install this plugin on your Bungeecord/Velocity server. Please read the installation guid (https://github.com/Simonsator/Party-API-For-Spigot/wiki/Installation)");
+			tableExists();
 		} catch (SQLException e) {
+			new ErrorReporter("§cYou did not install this plugin on your Bungeecord/Velocity server. Please read the installation guid (https://github.com/Simonsator/Party-API-For-Spigot/wiki/Installation)");
 			e.printStackTrace();
 		}
 	}
 
-	private boolean tableExists(Connection connection, String tableName) throws SQLException {
-		DatabaseMetaData meta = connection.getMetaData();
-		ResultSet resultSet = meta.getTables(null, null, tableName, new String[]{"TABLE"});
-		return resultSet.next();
+	private void tableExists() throws SQLException {
+		Connection con = getConnection();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			rs = (stmt = con.createStatement()).executeQuery("select * from `" + TABLE_PREFIX + "party` LIMIT 1");
+			rs.next();
+		} finally {
+			close(rs, stmt);
+		}
 	}
 
 	public MySQLPlayerParty getParty(int pPlayerId) {
@@ -37,8 +46,7 @@ public class MySQLConnection extends SQLCommunication {
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
-			rs = (stmt = con.createStatement()).executeQuery("select `player_member_id`, `leader_id` from `" + DATABASE +
-					"`.`" + TABLE_PREFIX + "party` WHERE leader_id= (SELECT leader_id FROM  `" + DATABASE + "`.`" + TABLE_PREFIX
+			rs = (stmt = con.createStatement()).executeQuery("select `player_member_id`, `leader_id` from `" + TABLE_PREFIX + "party` WHERE leader_id= (SELECT leader_id FROM  `" + TABLE_PREFIX
 					+ "party` WHERE player_member_id = '" + pPlayerId + "'LIMIT 1 )");
 			while (rs.next()) {
 				int memberId = rs.getInt("player_member_id");
