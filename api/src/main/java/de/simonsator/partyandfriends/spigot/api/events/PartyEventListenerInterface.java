8e93b1d0741c88b750ce2c8c8d52a76051b44fb7
Simonsator <00pflaume@gmail.com>
2023-03-27 15:24:54 +0200

    Better method naming

--- api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventListenerInterface.java
+++ api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventListenerInterface.java
@@ -2,15 +2,34 @@ package de.simonsator.partyandfriends.spigot.api.events;
 
 import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayer;
 import de.simonsator.partyandfriends.spigot.api.party.PlayerParty;
+import org.jetbrains.annotations.Nullable;
 
 
 public interface PartyEventListenerInterface {
+	/**
+	 * @param pPlayer The player who left the party
+	 * @param pParty  The party the player left. If they were the last player or the player deleted the party while this
+	 *                event was being bridged to the spigot server, then the party might be null.
+	 */
+	void onLeftParty(PAFPlayer pPlayer, @Nullable PlayerParty pParty);
 
-	void onLeftPartyEvent(PAFPlayer pPlayer, PlayerParty pParty);
+	/**
+	 * @param pParty The party that was created. If the player deleted the party while this event was being bridged to
+	 *               the spigot server, then the party might be null.
+	 */
+	void onPartyCreated(@Nullable PlayerParty pParty);
 
-	void onPartyCreatedEvent(PlayerParty pParty);
+	/**
+	 * @param pPlayer The player who joined the party
+	 * @param pParty  The party the player joined. If the player deleted the party while this event was being bridged to
+	 *                the spigot server, then the party might be null.
+	 */
+	void onPartyJoin(PAFPlayer pPlayer, @Nullable PlayerParty pParty);
 
-	void onPartyJoinEvent(PAFPlayer pPlayer, PlayerParty pParty);
-
-	void onPartyLeaderChanged(PAFPlayer pNewLeader, PlayerParty pParty);
+	/**
+	 * @param pNewLeader The new leader of the party
+	 * @param pParty     The party the leader changed for. If the player deleted the party while this event was being bridged to
+	 *                   the spigot server, then the party might be null.
+	 */
+	void onPartyLeaderChanged(PAFPlayer pNewLeader, @Nullable PlayerParty pParty);
 }
