f556089c3ed07f520369fe1b1029fc834cc85cd6
Simonsator <00pflaume@gmail.com>
2023-03-27 15:44:10 +0200

    Added javadoc

--- api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventListenerInterface.java
+++ api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventListenerInterface.java
@@ -4,9 +4,13 @@ import de.simonsator.partyandfriends.spigot.api.pafplayers.PAFPlayer;
 import de.simonsator.partyandfriends.spigot.api.party.PlayerParty;
 import org.jetbrains.annotations.Nullable;
 
-
+/**
+ * The interface to receive party events. Please register your listener with {@link PartyEventManager#registerPartyEventListener(PartyEventListenerInterface)}
+ */
 public interface PartyEventListenerInterface {
 	/**
+	 * This command will only be called if a party member or the person who left the party, is currently on this spigot server.
+	 *
 	 * @param pPlayer The player who left the party
 	 * @param pParty  The party the player left. If they were the last player or the player deleted the party while this
 	 *                event was being bridged to the spigot server, then the party might be null.
@@ -14,12 +18,16 @@ public interface PartyEventListenerInterface {
 	void onLeftParty(PAFPlayer pPlayer, @Nullable PlayerParty pParty);
 
 	/**
+	 * This command will only be called if a party member, is currently on this spigot server.
+	 *
 	 * @param pParty The party that was created. If the player deleted the party while this event was being bridged to
 	 *               the spigot server, then the party might be null.
 	 */
 	void onPartyCreated(@Nullable PlayerParty pParty);
 
 	/**
+	 * This command will only be called if a party member, is currently on this spigot server.
+	 *
 	 * @param pPlayer The player who joined the party
 	 * @param pParty  The party the player joined. If the player deleted the party while this event was being bridged to
 	 *                the spigot server, then the party might be null.
@@ -27,6 +35,8 @@ public interface PartyEventListenerInterface {
 	void onPartyJoin(PAFPlayer pPlayer, @Nullable PlayerParty pParty);
 
 	/**
+	 * This command will only be called if a party member, is currently on this spigot server.
+	 *
 	 * @param pNewLeader The new leader of the party
 	 * @param pParty     The party the leader changed for. If the player deleted the party while this event was being bridged to
 	 *                   the spigot server, then the party might be null.
--- api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventManager.java
+++ api/src/main/java/de/simonsator/partyandfriends/spigot/api/events/PartyEventManager.java
@@ -7,10 +7,23 @@ public abstract class PartyEventManager {
 		instance = pInstance;
 	}
 
+	/**
+	 * Checks if the bridge is available
+	 *
+	 * @return True if the bridge is available, false if not.
+	 */
 	public static boolean isBridgeAvailable() {
 		return instance != null;
 	}
 
+	/**
+	 * Registers a listener for party events
+	 *
+	 * @param pListener The listener Which will be registered
+	 * @throws BridgeNotAvailableException If the bridge is not available. This means that the GUI of Party And Friends
+	 *                                     is not installed on this spigot server. The GUI is required as it used as a
+	 *                                     bridge between the Spigot server and Bungeecord server.
+	 */
 	public static void registerPartyEventListener(PartyEventListenerInterface pListener) throws BridgeNotAvailableException {
 		if (instance == null)
 			throw new BridgeNotAvailableException();
