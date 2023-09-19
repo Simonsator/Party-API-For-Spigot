cf57dcd9fefd0c89b9a352464abc05adf7408b38
Simonsator <00pflaume@gmail.com>
2023-09-19 20:47:15 +0200

    Add user support

--- spigot-plugin/src/main/java/de/simonsator/partyandfriends/spigot/party/redis/RedisPartyManager.java
+++ spigot-plugin/src/main/java/de/simonsator/partyandfriends/spigot/party/redis/RedisPartyManager.java
@@ -6,6 +6,8 @@ import de.simonsator.partyandfriends.spigot.api.party.PlayerParty;
 import org.bukkit.configuration.Configuration;
 import redis.clients.jedis.Jedis;
 
+import java.util.Objects;
+
 public class RedisPartyManager extends PartyManager {
 	private final Configuration CONFIG;
 
@@ -15,8 +17,13 @@ public class RedisPartyManager extends PartyManager {
 
 	Jedis getConnection() {
 		Jedis jedis = new Jedis(CONFIG.getString("Redis.Host"), CONFIG.getInt("Redis.Port"));
-		if (!CONFIG.getString("Redis.Password").equals(""))
-			jedis.auth(CONFIG.getString("Redis.Password"));
+		if (!Objects.requireNonNull(CONFIG.getString("Redis.Password")).isEmpty()) {
+			if (!Objects.requireNonNull(CONFIG.getString("Redis.User")).isEmpty()) {
+				jedis.auth(CONFIG.getString("Redis.User"), CONFIG.getString("Redis.Password"));
+			} else {
+				jedis.auth(CONFIG.getString("Redis.Password"));
+			}
+		}
 		return jedis;
 	}
 
